48724e4f197e5e5754e08c4079351ca091a21f9d
Asliddin Abdivasiyev <asliddin.abdivasiyev@gmail.com>
2025-04-06 05:21:54 +0500

    fix tmux default shell

--- modules/home/tmux/default.nix
+++ modules/home/tmux/default.nix
@@ -2,6 +2,7 @@
   programs.tmux = {
     enable = true;
     keyMode = "vi";
+	terminal = "screen-256color";
     extraConfig = builtins.readFile ./tmux/tmux.conf;
     plugins = with pkgs; [
       tmuxPlugins.yank
