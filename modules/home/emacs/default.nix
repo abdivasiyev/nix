a1b1f724d42f2006bf38ba6122736edeefccdbd3
Asliddin Abdivasiyev <asliddin.abdivasiyev@gmail.com>
2025-12-29 12:00:57 +0500

    feat: add init.el

--- modules/home/emacs/default.nix
+++ modules/home/emacs/default.nix
@@ -3,5 +3,8 @@ in {
   programs.emacs = {
     enable = true;
     package = pkgs.emacs-gtk;
+    extraConfig = builtins.concatStringsSep "\n" [
+      (builtins.readFile ./elisp/init.lua)
+    ];
   };
 }
