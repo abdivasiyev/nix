3d4fb1d083153d80a4f565914a364b8d4471d92a
Asliddin Abdivasiyev <asliddin.abdivasiyev@gmail.com>
2025-12-30 12:06:38 +0500

    feat(emacs): setup .emacs.d configs

--- modules/home/emacs/default.nix
+++ modules/home/emacs/default.nix
@@ -1,10 +1,8 @@
 {pkgs, ...}: {
-  services.emacs = {
-    enable = true;
-  };
-
   programs.emacs = {
     enable = true;
     package = pkgs.emacs-gtk;
   };
+
+  home.file.".emacs.d".source = ./emacs.d;
 }
